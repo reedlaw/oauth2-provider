4c714ad4e13eca94a9223fa86234c0dc5b5eb2b7
Reed Law <reed@smashingboxes.com>
2012-03-01 16:41:32 -0500

    mongoid support

--- lib/oauth2/provider/models/access_token.rb
+++ lib/oauth2/provider/models/access_token.rb
@@ -11,8 +11,12 @@ module OAuth2::Provider::Models::AccessToken
     delegate :scope, :has_scope?, :client, :resource_owner, :to => :authorization
   end
 
-  def initialize(attributes = {}, options = {}, &block)
-    super attributes.reverse_merge(:access_token => self.class.unique_random_token(:access_token), :refresh_token => self.class.unique_random_token(:refresh_token)), options
+  def initialize(*args, &block)
+    super
+    self.access_token ||= self.class.unique_random_token(:access_token)
+    self.refresh_token ||= self.class.unique_random_token(:refresh_token)
+  # def initialize(attributes = {}, options = {}, &block)
+  #   super attributes.reverse_merge(:access_token => self.class.unique_random_token(:access_token), :refresh_token => self.class.unique_random_token(:refresh_token)), options
   end
 
   def as_json(options = {})
